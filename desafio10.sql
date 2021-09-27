b5c502f235a2e2c2056102d5ad89968e62efb601
matheustkaczyk <matheus111904@hotmail.com>
2021-09-27 18:45:57 -0300

    corrigindo tabs em todos os arquivos

--- desafio10.sql
+++ desafio10.sql
@@ -2,12 +2,12 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total INT;
-    SELECT
-    COUNT(*)
-    FROM SpotifyClone.historicoMusicas
-    WHERE usuario_id = id
-    INTO total;
-    RETURN total;
+DECLARE total INT;
+SELECT
+COUNT(*)
+FROM SpotifyClone.historicoMusicas
+WHERE usuario_id = id
+INTO total;
+RETURN total;
 END $$
 DELIMITER ;
--- desafio9.sql
+++ desafio9.sql
@@ -1,12 +1,12 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(50))
 BEGIN
-	SELECT
-    AR.nome_artista AS artista,
-    AL.album_name AS album
-    FROM SpotifyClone.artistas AR
-    JOIN SpotifyClone.albuns AL
-    ON AR.artista_id = AL.artista_id
-    WHERE AR.nome_artista = nome;
+SELECT
+AR.nome_artista AS artista,
+AL.album_name AS album
+FROM SpotifyClone.artistas AR
+JOIN SpotifyClone.albuns AL
+ON AR.artista_id = AL.artista_id
+WHERE AR.nome_artista = nome;
 END $$
 DELIMITER ;
