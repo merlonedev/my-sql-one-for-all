ddf443fdf25fb51f9f96d35ab1e7f481ff8ed278
Marcela Silva <marcela.ops@hotmail.br>
2021-09-27 20:27:01 -0300

    resolve desafio3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+	SELECT 
+		u.user_name AS 'usuario', 
+		so.song_name AS'nome' 
+	FROM SpotifyClone.reproduction_history AS rh
+	JOIN SpotifyClone.user AS u ON u.user_id = rh.user_id
+	JOIN SpotifyClone.song AS so ON so.song_id = rh.song_id
+	ORDER BY usuario, nome;
