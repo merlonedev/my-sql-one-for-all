cf636662ef699bec9571398de051c4f10bf7fd54
Merlone <alestori7@gmail.com>
2021-09-27 22:24:35 -0300

    Desafio 2 :white_check_mark:

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+CREATE View estatisticas_musicais AS
+SELECT 
+    (SELECT COUNT(*)
+    FROM SpotifyClone.musicas) AS `cancoes`, 
+    (SELECT COUNT(*)
+    FROM SpotifyClone.artistas) AS `artistas`, 
+    (SELECT COUNT(*)
+    FROM SpotifyClone.albuns) AS `albuns`; 
