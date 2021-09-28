321708b72f030be2eb797d1059d781bd0fe3a1dc
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-28 14:53:20 -0300

    desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,2 +1,10 @@
 CREATE VIEW perfil_artistas AS
-  
+  SELECT
+	art.name AS `artista`,
+  alb.name AS `album`,
+  COUNT(ua.artist_id) AS `seguidores`
+FROM SpotifyClone.albums AS alb
+JOIN SpotifyClone.artists AS art ON alb.artist_id = art.id
+JOIN SpotifyClone.user_artist AS ua ON alb.artist_id = ua.artist_id
+GROUP BY art.name, alb.name
+ORDER BY `seguidores` DESC, `artista` ASC, `album` ASC;
