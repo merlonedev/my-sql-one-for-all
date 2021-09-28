54a8b4bb53dcc976c37cb6b775b92ea492aca5a7
Uriel <urielbochi@gmail.com>
2021-09-28 15:23:13 -0300

    esqueci de por create view no 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 CREATE VIEW faturamento_atual AS
-SELECT MIN(p.plano_preco) faturamento_minimo, MAX(p.plano_preco) faturamento_maximo,
+SELECT MIN(p.plano_preco) AS `faturamento_minimo`, MAX(p.plano_preco) faturamento_maximo,
 ROUND(AVG(p.plano_preco), 2) faturamento_medio, SUM(p.plano_preco) faturamento_total
 FROM spotifyclone.plano p
 INNER JOIN spotifyclone.usuario u
