2c8659c8502731c24b804be6c48f6e48870a7581
RyanLaiber <ryanlaiber@gmail.com>
2021-09-28 17:51:08 -0300

    corrige req 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,11 @@
+USE SpotifyClone;
+
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM relacao_seguidores AS rs WHERE rs.usuario_id = OLD.usuario_id;
-DELETE FROM historico_reproducao AS hr WHERE hr.usuario_id = OLD.usuario_id;
+DELETE FROM relacao_seguidores AS rs WHERE usuario_id = OLD.usuario_id;
+DELETE FROM historico_reproducao AS hr WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
