9d86e96d2805fbdd7226a0237b59fba61f373275
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-28 19:46:12 -0300

    tenta corrigir desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,18 +1,18 @@
 CREATE VIEW SpotifyClone.faturamento_atual AS
     SELECT 
         (SELECT 
-                MIN(valor)
+                FORMAT(MIN(valor), 2)
             FROM
                 SpotifyClone.plano) AS faturamento_minimo,
         (SELECT 
-                MAX(valor)
+                FORMAT(MAX(valor), 2)
             FROM
                 SpotifyClone.plano) AS faturamento_maximo,
         (SELECT 
-                ROUND(SUM(valor) / 4, 2)
+                FORMAT(ROUND(SUM(valor) / 4, 2), 2)
             FROM
                 SpotifyClone.plano) AS faturamento_medio,
         (SELECT 
-                SUM(valor)
+                FORMAT(SUM(valor), 2)
             FROM
                 SpotifyClone.plano) AS faturamento_total;
