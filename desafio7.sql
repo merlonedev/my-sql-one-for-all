e85d86dfe178d1176dec64128e480bc2574cf185
Guilherme Cesconeto <cesconeto@gmail.com>
2021-09-28 19:52:21 -0300

    Fazendo a 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+CREATE VIEW perfil_artistas AS 
+  SELECT
+   ar.artist_name AS `artista`,
+   al.album_name AS `album`,
+   COUNT(ua.id_user) AS `seguidores`
+  FROM
+    SpotifyClone.user_artists AS ua
+  JOIN SpotifyClone.artists AS ar ON ua.id_artist = ar.id_artist
+  JOIN SpotifyClone.albums AS al ON ua.id_artist = al.id_artist
+  GROUP BY `artista`, `album`
+  ORDER BY `seguidores` DESC, `album`;
