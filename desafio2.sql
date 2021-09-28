e34b0312cb493b68eb76b7b92fcd600867e4640b
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-28 20:02:57 -0300

    Pretende resolver o requisito 2 [3]

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT
-(SELECT COUNT(*) FROM cancao) AS SpotifyClone.cancoes,
-(SELECT COUNT(*) FROM artista) AS SpotifyClone.artistas,
-(SELECT COUNT(*) FROM album) AS SpotifyClone.albuns;
+(SELECT COUNT(*) FROM SpotifyClone.cancao) AS cancoes,
+(SELECT COUNT(*) FROM SpotifyClone.artista) AS artistas,
+(SELECT COUNT(*) FROM SpotifyClone.album) AS albuns;
