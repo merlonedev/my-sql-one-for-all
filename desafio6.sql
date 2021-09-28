3d0cefcf541760ccd3cb6037206d6236c997e024
Ramon Paschoal <rpaschoal90@gmail.com>
2021-09-28 18:19:20 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS 
+SELECT
+MIN(p.price) AS faturamento_minimo,
+MAX(p.price) AS faturamento_maximo,
+ROUND(AVG(p.price), 2) AS faturamento_medio,
+SUM(p.price) AS faturamento_total
+FROM SpotifyClone.plans AS p
+INNER JOIN users AS u
+ON p.plan_id = u.plan_id;
