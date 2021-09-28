051402f86272b333e7e2fd1898f054cf8dc4e7b5
Luciano Lanes <llaneslopes@gmail.com>
2021-09-28 17:27:23 -0300

    FIX desafio 8  code climate

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
-
+-- Inspirado na resposta vista no link shorturl.at/cdghE
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.`user`
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.user_artist WHERE OLD.id = user_id;
-    DELETE FROM SpotifyClone.user_song WHERE OLD.id = user_id;
+DELETE FROM SpotifyClone.user_artist WHERE OLD.id = user_id;
+DELETE FROM SpotifyClone.user_song WHERE OLD.id = user_id;
 END $$
 
 DELIMITER ;
