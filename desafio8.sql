60f26fc94154223ac5ad5330d3fcdd0aa7515108
RyanLaiber <ryanlaiber@gmail.com>
2021-09-28 17:52:45 -0300

    corrige req 8

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM relacao_seguidores AS rs WHERE usuario_id = OLD.usuario_id;
-DELETE FROM historico_reproducao AS hr WHERE usuario_id = OLD.usuario_id;
+DELETE FROM relacao_seguidores WHERE usuario_id = OLD.usuario_id;
+DELETE FROM historico_reproducao WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
