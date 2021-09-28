99cc91ede0d9ea8cfd5871ffc9414f80baea2271
tiagoornelas <advtiagoornelas@gmail.com>
2021-09-28 20:50:03 -0300

    Pretende resolver o requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT C.nome AS cancao, COUNT(UOC.id_usuario) AS reproducoes
+FROM SpotifyClone.usuario_ouviu_cancao UOC
+INNER JOIN SpotifyClone.cancao C
+ON C.id_cancao = UOC.id_cancao
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
