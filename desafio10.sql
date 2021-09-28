6db123cb2c6e83e849e026de086ce8a7e8915ddd
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-28 17:06:03 -0300

    add desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,17 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico (nome_usuario VARCHAR(20))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_musicas INT;
+SELECT COUNT(cancao_id) FROM SpotifyClone.cancoes_reproduzidas_usuario AS cru
+INNER JOIN usuarios AS u
+ON u.usuario_id = cru.usuario_id
+WHERE u.usuario = nome_usuario
+INTO total_musicas;
+RETURN total_musicas;
+END $$
+
+DELIMITER ;
