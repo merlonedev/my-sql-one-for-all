b1d903cf4f65b68af9c52bbd7ab9eae44b1ee10f
Ramon Paschoal <rpaschoal90@gmail.com>
2021-09-28 17:52:32 -0300

    Requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+CREATE VIEW historico_reproducao_usuarios AS 
+SELECT
+u.`name` AS usuario,
+s.title AS nome
+FROM SpotifyClone.users as u
+INNER JOIN SpotifyClone.`history` AS h
+ON u.user_id = h.user_id
+INNER JOIN SpotifyClone.songs AS s
+ON s.song_id = h.song_id
+ORDER BY usuario, nome;
