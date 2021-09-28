a35dd8df9d16e9d2725a3b41515a84021b548d03
Gabriel Takaki <gabitj@icloud.com>
2021-09-28 16:38:59 -0300

    corrige erro unexpected tabs found

--- desafio1.sql
+++ desafio1.sql
@@ -13,9 +13,9 @@ CREATE TABLE Plano (
 
 INSERT INTO Plano (valor_plano, plano)
 VALUES
-	(0, 'Gratuito'),
-	(7.99, 'Familiar'),
-	(5.99, 'Universitário');
+(0, 'Gratuito'),
+(7.99, 'Familiar'),
+(5.99, 'Universitário');
 
 CREATE TABLE Artista (
     id INT NOT NULL AUTO_INCREMENT,
@@ -25,10 +25,10 @@ CREATE TABLE Artista (
 
 INSERT INTO Artista (artista_nome)
 VALUES
-	('Walter Phoenix'),
-	('Peter Strong'),
-	('Lance Day'),
-	('Freedie Shannon');
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
 
 CREATE TABLE Album (
@@ -42,11 +42,11 @@ CREATE TABLE Album (
 
 INSERT INTO Album (album, artista_id)
 VALUES
-	('Envious', 1),
-	('Exuberant', 1),
-	('Hallowed Steam', 2),
-	('Incandescent', 3),
-	('Temporary Culture', 4);
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 CREATE TABLE Usuario (
     id INT NOT NULL AUTO_INCREMENT,
@@ -60,10 +60,10 @@ CREATE TABLE Usuario (
 
 INSERT INTO Usuario (usuario, idade, plano_id)
 VALUES
-	('Thati', 23, 1),
-	('Cintia', 35, 2),
-	('Bill', 20, 3),
-	('Roger', 45, 1);
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
 
 CREATE TABLE Musicas (
     id INT NOT NULL AUTO_INCREMENT,
@@ -79,24 +79,24 @@ CREATE TABLE Musicas (
 
 INSERT INTO Musicas (artista_id, album_id, musica)
 VALUES
-	(1, 1, 'Soul For Us'),
-	(1, 1, 'Reflections Of Magic'),
-	(1, 1, 'Dance With Her Own'),
-	(1, 2, 'Troubles Of My Inner Fire'),
-	(1, 2, 'Time Fireworks'),
-	(2, 3, 'Magic Circus'),
-	(2, 3, 'Honey, So Do I'),
-	(2, 3, "Sweetie, Let's Go Wild"),
-	(2, 3, 'She Knows'),
-	(3, 4, 'Fantasy For Me'),
-	(3, 4, 'Celebration Of More'),
-	(3, 4, 'Rock His Everything'),
-	(3, 4, 'Home Forever'),
-	(3, 4, 'Diamond Power'),
-	(3, 4, "Honey, Let's Be Silly"),
-	(4, 5, 'Thang Of Thunder'),
-	(4, 5, 'Words Of Her Life'),
-	(4, 5, 'Without My Streets');
+(1, 1, 'Soul For Us'),
+(1, 1, 'Reflections Of Magic'),
+(1, 1, 'Dance With Her Own'),
+(1, 2, 'Troubles Of My Inner Fire'),
+(1, 2, 'Time Fireworks'),
+(2, 3, 'Magic Circus'),
+(2, 3, 'Honey, So Do I'),
+(2, 3, "Sweetie, Let's Go Wild"),
+(2, 3, 'She Knows'),
+(3, 4, 'Fantasy For Me'),
+(3, 4, 'Celebration Of More'),
+(3, 4, 'Rock His Everything'),
+(3, 4, 'Home Forever'),
+(3, 4, 'Diamond Power'),
+(3, 4, "Honey, Let's Be Silly"),
+(4, 5, 'Thang Of Thunder'),
+(4, 5, 'Words Of Her Life'),
+(4, 5, 'Without My Streets');
 
 CREATE TABLE Seguindo_artistas (
     usuario_id INT NOT NULL,
@@ -110,14 +110,14 @@ CREATE TABLE Seguindo_artistas (
 
 INSERT INTO Seguindo_artistas (usuario_id, artista_id)
 VALUES
-	(1, 1),
-	(1, 4),
-	(1, 3),
-	(2, 1),
-	(2, 3),
-	(3, 2),
-	(3, 1),
-	(4, 4);
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
 
 CREATE TABLE Historico_reproducao (
     usuario_id INT NOT NULL,
@@ -131,17 +131,17 @@ CREATE TABLE Historico_reproducao (
 
 INSERT INTO Historico_reproducao (usuario_id, musica_id)
 VALUES
-	(1, 1),
-	(1, 6),
-    (1, 14),
-    (1, 16),
-	(2, 13),
-	(2, 17),
-	(2, 2),
-	(2, 15),
-	(3, 4),
-	(3, 16),
-	(3, 6),
-	(4, 3),
-	(4, 18),
-	(4, 11);
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
