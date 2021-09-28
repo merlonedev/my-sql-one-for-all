9d8abe48dec537e4408f133826b15fd263e7a1a5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-09-28 17:26:17 -0300

    cria a tabela plans

--- desafio1.sql
+++ desafio1.sql
@@ -8,3 +8,9 @@ CREATE TABLE users(
   age INT NOT NULL,
   plan_id INT NOT NULL
 ) engine = innoDB;
+
+CREATE TABLE plans(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  plan_name VARCHAR(50) NOT NULL,
+  plan_value DOUBLE
+) engine = innoDB;
