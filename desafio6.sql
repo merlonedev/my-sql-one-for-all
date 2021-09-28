7ef0ad1e91e9ab39b69cd1569be765e9e96ba46a
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-09-28 12:52:55 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        ROUND(MIN(price), 2) AS faturamento_minimo,
+        ROUND(MAX(price), 2) AS faturamento_maximo,
+        ROUND(AVG(price), 2) AS faturamento_medio,
+        ROUND(SUM(price), 2) AS faturamento_total
+    FROM
+        SpotfiyClone.plans as p
+        inner join SpotfiyClone.users as u on p.plan_id = u.plan_id;
