d55a171fc8f7ab1506c0753b4f2bd5fc59b82d33
julio11lp <julio11lp@hotmail.com>
2021-09-28 18:09:06 -0300

    Desafio 8 para testar no avaluator do git

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,10 @@
-DELIMITER $$
+DELIMITER  $$ 
+
 CREATE TRIGGER trigger_usuario_delete
-BEFORE DELETE ON SpotifyClone.Usuario 
-FOR EACH ROW
+  BEFORE DELETE ON SpotifyClone.Usuario
+  FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.Seguindo WHERE OLD.usuario_id = SpotifyClone.Seguindo.usuario_id;
-DELETE FROM SpotifyClone.Historico WHERE OLD.usuario_id = SpotifyClone.Seguindo.usuario_id;
+  DELETE FROM Seguindo WHERE OLD.usuario_id = usuario_id;
+  DELETE FROM Historico WHERE OLD.usuario_id = usuario_id;
 END $$
 DELIMITER ;
