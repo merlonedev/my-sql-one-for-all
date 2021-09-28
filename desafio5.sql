d3570ca07becc121b33e80bea9490cbf1e9c4015
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-28 17:46:17 -0300

    Resolve requisito 5(ajustes no code climate)

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,10 @@
 CREATE VIEW top_2_hits_do_momento AS
-	(SELECT 
-		m.nome AS 'cancao', COUNT(uh.musica_id) AS 'reproducoes'
-	FROM
-		SpotifyClone.musicas AS m
-			JOIN
-		SpotifyClone.usuario_historico AS uh ON uh.musica_id = m.musica_id
-	GROUP BY m.nome
-	ORDER BY COUNT(uh.musica_id) DESC , m.nome
-	LIMIT 2);
+(SELECT 
+  m.nome AS 'cancao', COUNT(uh.musica_id) AS 'reproducoes'
+FROM
+  SpotifyClone.musicas AS m
+    JOIN
+  SpotifyClone.usuario_historico AS uh ON uh.musica_id = m.musica_id
+GROUP BY m.nome
+ORDER BY COUNT(uh.musica_id) DESC , m.nome
+LIMIT 2);
