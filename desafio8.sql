e0d2193861849e850f26bbfc1b3727da1e99b11b
cristianocsl <cslcristiano@gmail.com>
2021-09-28 20:22:48 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+    AFTER DELETE ON usuarios
+    FOR EACH ROW
+BEGIN
+    DELETE FROM artistas_usuarios WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM histor_reprod WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
