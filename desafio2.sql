a1523cc8011babdac48a6eb709045402fbc23dd6
Caio Morato <morato_dev@protonmail.com>
2021-09-28 07:40:40 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        COUNT(DISTINCT mu.nome_musica) AS cancoes,
+        COUNT(DISTINCT ar.nome_artista) AS artistas,
+        COUNT(DISTINCT al.nome_album) AS albuns
+    FROM
+        musicas AS mu
+            JOIN
+        albuns AS al ON al.album_id = mu.album_id
+            JOIN
+        artistas AS ar ON al.artista_id = ar.artista_id;
+        
