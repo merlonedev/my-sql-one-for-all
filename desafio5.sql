540318e9d0e80b67517656218b7e387c6a5cbd60
Pedro <peg.gep27@gmail.com>
2021-09-28 19:04:32 -0300

    desafio 2 a 6

---
+++ desafio5.sql
@@ -0,0 +1,13 @@
+CREATE VIEW top_2_hits_do_momento AS
+    (SELECT 
+        m.musica AS cancao,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.historico_musicas hm
+            WHERE
+                hm.id_musica = m.id_musica) AS reproducoes
+    FROM
+        SpotifyClone.musicas m
+    ORDER BY 2 DESC , 1
+    LIMIT 2);
