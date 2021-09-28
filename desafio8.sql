4ce1298c5c5fd6a333a614673dc68bf6135575fc
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-09-28 18:39:31 -0300

    feat challenges 8 and 9

---
+++ desafio8.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE
+TRIGGER	trigger_usuario_delete
+BEFORE DELETE ON
+	usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM
+  SpotifyClone.reproducoes
+WHERE
+  usuario_id = OLD.usuario_id;
+DELETE FROM
+  SpotifyClone.seguindo
+WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
---
+++ desafio9.sql
@@ -0,0 +1,22 @@
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(32))
+BEGIN
+SELECT
+  ar.artista
+AS
+  `artista`,
+  al.album
+AS
+  `album`
+FROM
+  SpotifyClone.artistas ar
+INNER JOIN
+  SpotifyClone.albuns al
+ON
+  al.artista_id = ar.artista_id
+WHERE
+  ar.artista = nome_artista;
+END $$
+
+DELIMITER ;
