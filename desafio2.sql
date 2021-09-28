7e54253c391ee34deca2ac2856b183f4a209bc41
Merlone <alestori7@gmail.com>
2021-09-28 14:30:45 -0300

    fix: erros de code climate

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,14 @@
-CREATE View estatisticas_musicais AS
-SELECT 
-    (SELECT COUNT(*)
-    FROM SpotifyClone.musicas) AS `cancoes`, 
-    (SELECT COUNT(*)
-    FROM SpotifyClone.artistas) AS `artistas`, 
-    (SELECT COUNT(*)
-    FROM SpotifyClone.albuns) AS `albuns`; 
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.musicas) AS `cancoes`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.artistas) AS `artistas`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.albuns) AS `albuns`; 
--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,10 @@
-CREATE View historico_reproducao_usuarios AS 
-    (SELECT u.usuario,
-         m.musica `nome`
-    FROM SpotifyClone.usuarios u
-    JOIN SpotifyClone.historico_musicas hm
-        ON hm.id_usuario = u.id_usuario
-    JOIN SpotifyClone.musicas m
-        ON m.id_musica = hm.id_musica
-    ORDER BY  1, 2);
+CREATE VIEW historico_reproducao_usuarios AS
+    (SELECT 
+        u.usuario, m.musica `nome`
+    FROM
+        SpotifyClone.usuarios u
+            JOIN
+        SpotifyClone.historico_musicas hm ON hm.id_usuario = u.id_usuario
+            JOIN
+        SpotifyClone.musicas m ON m.id_musica = hm.id_musica
+    ORDER BY 1 , 2);
