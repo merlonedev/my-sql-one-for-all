361c908cac47ce51cc382220f8f2a0c3a7c250f3
julio11lp <julio11lp@hotmail.com>
2021-09-27 22:47:15 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        COUNT(musica_id) AS `cancoes`,
+        COUNT(DISTINCT artista_id) AS `artistas`,
+        COUNT(DISTINCT album_id) AS `albuns`
+    FROM
+        SpotifyClone.Musica;
