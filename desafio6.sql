5f98b8b5f834ac3387be95d1b2480d3d4206c268
julio11lp <julio11lp@hotmail.com>
2021-09-27 23:32:51 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        ROUND(MIN(pl.plano_preco), 2) AS `faturamento_minimo`,
+        ROUND(MAX(pl.plano_preco), 2) AS `faturamento_maximo`,
+        ROUND(AVG(pl.plano_preco), 2) AS `faturamento_medio`,
+        ROUND(SUM(pl.plano_preco), 2) AS `faturamento_total`
+    FROM
+        Plano AS pl
+ INNER JOIN
+        SpotifyClone.Usuario AS us ON us.plano_id = pl.plano_id;
