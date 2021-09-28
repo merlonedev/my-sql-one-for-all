bb7998044d41e63be6114c655312048bfa7ee8a4
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-28 17:02:07 -0300

    Requisito 10 fixed

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION `quantidade_musicas_no_historico`(usuario VARCHAR(50)) RETURNS int
+CREATE FUNCTION `quantidade_musicas_no_historico`(usuario INT) RETURNS int
     READS SQL DATA
 BEGIN
 DECLARE result INT;
@@ -6,7 +6,7 @@ SELECT
 COUNT(exibicoes.id_usuario) 
 FROM tb_cancoes_usuario AS `exibicoes` 
 INNER JOIN tb_usuarios AS usuarios ON usuarios.usuario_id = exibicoes.id_usuario
-WHERE usuarios.usuario = usuario
+WHERE usuarios.usuario_id = usuario
 GROUP BY exibicoes.id_usuario
 INTO result;
 RETURN result;
