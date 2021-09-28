b27144b7f536e058b19b0ab4fd28245baa81fdbd
Armando <a.guerra58@hotmail.com>
2021-09-28 14:57:12 -0300

    desafio5

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        C.cancao_nome AS cancao, COUNT(H.usuario_id) AS reproducoes
+    FROM
+        SpotifyClone.Cancoes AS C
+            INNER JOIN
+        SpotifyClone.Historico AS H ON C.cancao_id = H.cancao_id
+    GROUP BY C.cancao_nome
+    ORDER BY `reproducoes` DESC , C.cancao_nome
+    LIMIT 2;
