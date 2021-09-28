05de45ef69f79baa521b26414c543d65a9511c99
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-28 17:46:27 -0300

    adiciona desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,21 @@
+drop view if exists SpotifyClone.estatisticas_musicais;
+
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+    SELECT 
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.cancao) AS cancoes,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.artista) AS artistas,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.album) AS albuns;
+
+SELECT 
+    *
+FROM
+    SpotifyClone.estatisticas_musicais;
