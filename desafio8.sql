d8f4e321decc424449141ee0053ae74937e02dad
Daniel <danielbped@gmail.com>
2021-09-28 18:21:52 -0300

    climate

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-	DELETE FROM usuario_historico WHERE usuario_id = old.id;
-    DELETE FROM usuario_artista WHERE usuario_id = old.id;
+DELETE FROM usuario_historico WHERE usuario_id = old.id;
+DELETE FROM usuario_artista WHERE usuario_id = old.id;
 END $$;
 DELIMITER ;
