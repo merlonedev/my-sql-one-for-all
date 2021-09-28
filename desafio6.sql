0d8f81973f269e6c33650ec344c936ccbaec185e
AndreHammel <hammel.engineer@gmail.com>
2021-09-28 19:52:15 -0300

    Update desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
 SELECT 
 MIN(p.valor) AS faturamento_minimo,
 MAX(p.valor) AS faturamento_maximo,
-concat(round(avg(p.valor), 2), '0') AS faturamento_medio,
+concat(round(avg(p.valor), 2), '') AS faturamento_medio,
 concat(round(SUM(p.valor),2), '') AS faturamento_total
 FROM
 USUARIO u
