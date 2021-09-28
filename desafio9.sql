c2252a4179e118271da60521b5db60c452574198
Kevin Oliveira <kevin.zero@hotmail.com>
2021-09-28 18:41:37 -0300

    11 - Cria procedure albuns_do_artista

---
+++ desafio9.sql
@@ -0,0 +1,20 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(30))
+BEGIN
+    SELECT 
+    art.artista, alb.album
+FROM
+    albuns AS alb
+        INNER JOIN
+    (SELECT 
+        *
+    FROM
+        artistas
+    WHERE
+        artista = nome) AS art ON art.id = alb.artista_id 
+	ORDER BY album;
+END $$
+
+DELIMITER ;
