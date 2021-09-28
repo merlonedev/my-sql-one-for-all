f3798491af10b3980d6ae9669c47054d236bc05d
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-09-28 17:24:28 -0400

    corrige code climate

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 CREATE VIEW estatisticas_musicais AS 
-	SELECT
-		COUNT(DISTINCT m.nome) AS 'cancoes',
-        COUNT(DISTINCT a.nome) AS 'artistas',
-        COUNT(DISTINCT al.nome) AS 'albuns'
-	FROM
-		musicas m,
-        artistas a,
-        albuns al;
+SELECT
+    COUNT(DISTINCT m.nome) AS 'cancoes',
+    COUNT(DISTINCT a.nome) AS 'artistas',
+    COUNT(DISTINCT al.nome) AS 'albuns'
+FROM
+    musicas m,
+    artistas a,
+    albuns al;
