612bec501413536bf59f89e1cc35f2fbb4c34cf2
Guilherme Dias <gmd.delta@gmail.com>
2021-09-28 20:41:24 -0300

    [PT-BR]Resolvido requisito 3. [EN] Solved 3rd requirement

---
+++ desafio3.sql
@@ -0,0 +1,18 @@
+CREATE VIEW historico_reproducao_usuarios AS
+    SELECT 
+        (SELECT 
+                t2.UserName
+            FROM
+                users AS t2
+            WHERE
+                t1.User_ID = t2.User_ID) AS 'usuario',
+        (SELECT 
+                t2.Song
+            FROM
+                songs AS t2
+            WHERE
+                t1.Song_ID = t2.Song_ID) AS 'nome'
+    FROM
+        user_songs_listened AS t1
+    ORDER BY 1 , 2;
+    
