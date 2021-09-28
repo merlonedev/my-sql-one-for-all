24324d0892d9590832ff11fdeafe00fb48ba6313
Bruno Augusto Claro <brunaugusto@gmail.com>
2021-09-28 15:42:33 -0300

    Fixed again 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,11 +1,10 @@
 CREATE VIEW top_3_artistas AS
 SELECT 
-artistas.nome_artista AS 'artista',
-COUNT(seguidores.id_usuario) AS 'seguidores'
+artistas.nome_artista AS `artista`,
+COUNT(seguidores.id_usuario) AS `seguidores`
 FROM
 tb_artistas_usuarios AS seguidores
-JOIN tb_usuarios AS usuarios ON usuarios.usuario_id = seguidores.id_usuario
 JOIN tb_artistas AS artistas ON artistas.id_artista = seguidores.id_artista
-GROUP BY artistas.nome_artista
-ORDER BY COUNT(seguidores.id_usuario) DESC , artistas.nome_artista ASC
+GROUP BY `artista`
+ORDER BY `seguidores` DESC , `artista` ASC
 LIMIT 3;
