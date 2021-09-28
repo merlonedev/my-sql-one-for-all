ecdb4fc6e90d39ab90c82a626454b785e07d999e
Daniel <danielbped@gmail.com>
2021-09-28 18:28:10 -0300

    Dclimateeee

--- desafio10.sql
+++ desafio10.sql
@@ -3,12 +3,12 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(usuarioId INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE numeroDeMusicas INT;
-	SELECT COUNT(*) AS `quantidade_musicas_no_historico`
-    FROM usuario_historico
-    WHERE usuario_id = usuarioId
-    INTO numeroDeMusicas;
-    RETURN numeroDeMusicas;
+DECLARE numeroDeMusicas INT;
+SELECT COUNT(*) AS `quantidade_musicas_no_historico`
+FROM usuario_historico
+WHERE usuario_id = usuarioId
+INTO numeroDeMusicas;
+RETURN numeroDeMusicas;
 END $$
 
 DELIMITER ;
