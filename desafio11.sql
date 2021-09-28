8ba29e39d22179ca4f169a2f0959141a2475b8c9
Uriel <urielbochi@gmail.com>
2021-09-28 17:07:02 -0300

    alteração no json

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,4 @@
+CREATE VIEW cancoes_premium AS
 SELECT m.musica_nome as 'nome', COUNT(h.usuario_id) AS `reproducoes` from spotifyclone.historico as h
 INNER JOIN spotifyclone.musica AS m ON m.musica_id = h.musica_id
 INNER JOIN spotifyclone.usuario AS us ON us.usuario_id = h.usuario_id
