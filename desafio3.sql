97e4ad373f5f76fae92cf59142e55c4a6f7ede87
AndreHammel <hammel.engineer@gmail.com>
2021-09-28 19:42:27 -0300

    Update desafio3.sql
    tirado os tabs

--- desafio3.sql
+++ desafio3.sql
@@ -1,11 +1,11 @@
 CREATE VIEW historico_reproducao_usuarios AS
-    SELECT 
-        u.usuario AS usuario, ca.nome_cancao AS nome
-    FROM
-        USUARIO u
-            JOIN
-        USUARIO_MUSICAS um ON u.usuario_id = um.usuario_id
-            JOIN
-        CANCOES ca ON um.musica_id = ca.cancoes_id
-    ORDER BY u.usuario , ca.nome_cancao;
-	
+SELECT 
+u.usuario AS usuario, ca.nome_cancao AS nome
+FROM
+USUARIO u
+JOIN
+USUARIO_MUSICAS um ON u.usuario_id = um.usuario_id
+JOIN
+CANCOES ca ON um.musica_id = ca.cancoes_id
+ORDER BY u.usuario , ca.nome_cancao;
+    
