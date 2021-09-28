6f8f5613bff6c00d014bc22f98a06590a5bc7c16
Merlone <alestori7@gmail.com>
2021-09-27 22:35:48 -0300

    Desafio 3 :white_check_mark:

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+CREATE View historico_reproducao_usuarios AS 
+    (SELECT u.usuario,
+         m.musica `nome`
+    FROM SpotifyClone.usuarios u
+    JOIN SpotifyClone.historico_musicas hm
+        ON hm.id_usuario = u.id_usuario
+    JOIN SpotifyClone.musicas m
+        ON m.id_musica = hm.id_musica
+    ORDER BY  1, 2);
