ca1160435b0189fa8006bdb59d4488c46a01b1b2
Ramon Paschoal <rpaschoal90@gmail.com>
2021-09-28 18:30:20 -0300

    Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -7,13 +7,3 @@ SUM(p.price) AS faturamento_total
 FROM SpotifyClone.plans AS p
 INNER JOIN users AS u
 ON p.plan_id = u.plan_id;
-
-
-SELECT
-MIN(p.price) AS faturamento_minimo,
-MAX(p.price) AS faturamento_maximo,
-ROUND(AVG(p.price), 2) AS faturamento_medio,
-SUM(p.price) AS faturamento_total
-FROM SpotifyClone.plans AS p
-INNER JOIN SpotifyClone.users AS u
-ON p.plan_id = u.plan_id;
