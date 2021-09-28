45281eae41056fe2b7d6cbda9bed9f0ba596babe
lucasnahas <lucasahas@gmail.com>
2021-09-28 23:31:49 +0100

    requisito 3 fix

--- requisito3.sql
+++ requisito3.sql
@@ -1,11 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS
     SELECT 
-    usuarios.usuario, cancoes.cancao
-FROM
-    usuariosCancoes
-        JOIN
-    usuarios ON usuarios.usuario_id = usuariosCancoes.usuario_id
-        JOIN
-    cancoes ON cancoes.cancao_id = usuariosCancoes.cancao_id
-ORDER BY usuarios.usuario , cancoes.cancao;
-       
+        usuarios.usuario, cancoes.cancao
+    FROM
+        usuariosCancoes
+            JOIN
+        usuarios ON usuarios.usuario_id = usuariosCancoes.usuario_id
+            JOIN
+        cancoes ON cancoes.cancao_id = usuariosCancoes.cancao_id
+    ORDER BY usuarios.usuario , cancoes.cancao;
