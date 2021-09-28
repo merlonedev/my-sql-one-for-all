5f2147a14dfb7b254ca0aff6edebd1d546e94d98
Uriel <urielbochi@gmail.com>
2021-09-28 17:13:19 -0300

    requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,11 +1,11 @@
 CREATE VIEW top_2_hits_do_momento AS
     SELECT 
         m.musica_nome AS `cancao`,
-        COUNT(m.musica_id) AS `reproducoes`
+        COUNT(sh.musica_id) AS `reproducoes`
     FROM
-        SpotifyClone.musica AS m
+        spotifyclone.musica AS m
             INNER JOIN
-        SpotifyClone.historico AS sh ON m.musica_id = sh.musica_id
+        spotifyclone.historico AS sh ON m.musica_id = sh.musica_id
     GROUP BY `cancao`
     ORDER BY `reproducoes` DESC , `cancao`
     LIMIT 2;
