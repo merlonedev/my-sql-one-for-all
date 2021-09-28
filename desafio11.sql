c95d7dc5162eb294f64b220fa7ac3da5dfd55944
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-09-28 17:04:34 -0300

    fix

--- desafio11.sql
+++ desafio11.sql
@@ -5,5 +5,9 @@ CREATE VIEW cancoes_premium AS
         songs s
             JOIN
         user_songs_history us ON us.song_id = s.song_id
+            JOIN
+        users u ON u.user_id = us.user_id
+    WHERE
+        u.plan_id IN (2 , 3)
     GROUP BY s.song_name
-    ORDER BY s.song_name;);
+    ORDER BY s.song_name);
