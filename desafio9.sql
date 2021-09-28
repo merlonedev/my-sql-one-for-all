117481123501daf8f3549f3c6681c09311a9a56d
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-28 19:27:58 -0300

    Resolve requisito 9(ajustes no code climate)

--- desafio9.sql
+++ desafio9.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(50))
 BEGIN
-	SELECT a.nome AS 'artista', al.nome AS 'album'
+SELECT a.nome AS 'artista', al.nome AS 'album'
     FROM SpotifyClone.artista AS a
     JOIN
     SpotifyClone.albuns AS al ON a.artista_id = al.artista_id
