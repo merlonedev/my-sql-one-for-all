58b262e3ba96f714a71f498a9f956a833f1b7baf
Jossany Moura <mjossany@gmail.com>
2021-09-28 20:35:02 -0300

    Corrige codeclimate desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -5,7 +5,7 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE planos(
-	plano_id INT PRIMARY KEY AUTO_INCREMENT,
+plano_id INT PRIMARY KEY AUTO_INCREMENT,
     tipo VARCHAR(20) NOT NULL,
     valor DECIMAL(4, 2)
 );
@@ -14,7 +14,7 @@ INSERT INTO planos(tipo, valor)
 VALUES('gratuito', 0), ('universitário', 5.99), ('familiar', 7.99);
 
 CREATE TABLE usuarios(
-	usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+usuario_id INT PRIMARY KEY AUTO_INCREMENT,
     nome_usuario VARCHAR(20) NOT NULL,
     idade INT NOT NULL,
     plano_id INT NOT NULL,
@@ -25,7 +25,7 @@ INSERT INTO usuarios(nome_usuario, idade, plano_id)
 VALUES ('Thati', 23, 1), ('Cintia', 35, 3), ('Bill', 20, 2), ('Roger', 45, 1);
 
 CREATE TABLE artistas(
-	artista_id INT PRIMARY KEY AUTO_INCREMENT,
+artista_id INT PRIMARY KEY AUTO_INCREMENT,
     artista_name VARCHAR(30) NOT NULL
 );
 
@@ -33,7 +33,7 @@ INSERT INTO artistas(artista_name)
 VALUES('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
 
 CREATE TABLE albuns(
-	album_id INT PRIMARY KEY AUTO_INCREMENT,
+album_id INT PRIMARY KEY AUTO_INCREMENT,
     album_name VARCHAR(50) NOT NULL,
     artista_id INT NOT NULL,
     FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
@@ -44,7 +44,7 @@ VALUES('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 
 
 
 CREATE TABLE musicas(
-	musica_id INT PRIMARY KEY AUTO_INCREMENT,
+musica_id INT PRIMARY KEY AUTO_INCREMENT,
     musica_name VARCHAR(30) NOT NULL,
     album_id INT NOT NULL,
     artista_id INT NOT NULL,
@@ -56,7 +56,7 @@ INSERT INTO musicas(musica_name, album_id, artista_id)
 VALUES ('Soul For Us', 1, 1), ('Reflections Of Magic', 1, 1), ('Dance With Her Own', 1, 1), ('Troubles Of My Inner Fire', 2, 1), ('Time Fireworks', 2, 1), ('Magic Circus', 3, 2), ('Honey, So Do I', 3, 2), ('Sweetie, Let\'s Go Wild', 3, 2), ('She Knows', 3, 2), ('Fantasy For Me', 4, 3), ('Celebration Of More', 4, 3), ('Rock His Everything', 4, 3), ('Home Forever', 4, 3), ('Diamond Power', 4, 3), ('Honey, Let\'s Be Silly', 4, 3), ('Thang Of Thunder', 5, 4), ('Words Of Her Life', 5, 4), ('Without My Streets', 5, 4);
 
 CREATE TABLE usuario_artistas_seguidos(
-	usuario_id INT,
+usuario_id INT,
     artista_id INT,
     PRIMARY KEY(usuario_id, artista_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
@@ -67,7 +67,7 @@ INSERT INTO usuario_artistas_seguidos(usuario_id, artista_id)
 VALUES(1, 1), (1, 4), (1, 3), (2, 1), (2, 3), (3, 2), (3, 1), (4, 4);
 
 CREATE TABLE usuario_historico_reproducoes(
-	usuario_id INT,
+usuario_id INT,
     musica_id INT,
     PRIMARY KEY(usuario_id, musica_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
