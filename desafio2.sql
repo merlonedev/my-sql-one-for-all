4531011a65372b0e30e6832a4f5a1999a461460d
Isaac Cruz <isaac.clm1@gmail.com>
2021-09-28 16:39:51 -0300

    Requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT 
-(SELECT COUNT(*) FROM SpotifyClone.musics) cancoes,
-(SELECT COUNT(*) FROM SpotifyClone.artists) artistas,
-(SELECT COUNT(*) FROM SpotifyClone.albums) albuns;
+(SELECT COUNT(*) FROM SpotifyClone.cancoes) AS cancoes,
+(SELECT COUNT(*) FROM SpotifyClone.artistas) AS artistas,
+(SELECT COUNT(*) FROM SpotifyClone.albums) AS albuns;
