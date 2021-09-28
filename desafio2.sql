2c9678698c2397e6af44c95c01f59f07cf915c8e
Armando <a.guerra58@hotmail.com>
2021-09-28 15:04:27 -0300

    fix desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -2,7 +2,7 @@ CREATE VIEW estatisticas_musicais AS
 SELECT 
 (SELECT COUNT(cancao_id) FROM SpotifyClone.Cancoes) AS 'cancoes',
 (SELECT COUNT(artista_id) FROM SpotifyClone.Artista) AS 'artistas',
-(SELECT COUNT(album_id) FROM SpotifyClone.Album) AS 'albuns';
+(SELECT COUNT(album_id) FROM SpotifyClone.Album) AS 'albuns'
 
 SELECT 
     *
