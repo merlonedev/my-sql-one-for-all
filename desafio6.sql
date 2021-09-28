9d5b14a3ab4ea2378f4e9db7d54ef337a9b767f3
Uriel <urielbochi@gmail.com>
2021-09-28 15:27:31 -0300

    serio..... tentativa de re-identação

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,10 @@
 CREATE VIEW faturamento_atual AS
-SELECT MIN(p.plano_preco) AS `faturamento_minimo`, MAX(p.plano_preco) faturamento_maximo,
-ROUND(AVG(p.plano_preco), 2) faturamento_medio, SUM(p.plano_preco) faturamento_total
-FROM spotifyclone.plano p
-INNER JOIN spotifyclone.usuario u
-ON p.plano_id = u.plano_id;
+    SELECT 
+        MIN(pl.plano_preco) AS faturamento_minimo,
+        MAX(pl.plano_preco) AS faturamento_maximo,
+        ROUND(AVG(pl.plano_preco), 2) AS faturamento_medio,
+        SUM(pl.plano_preco) AS faturamento_total
+    FROM
+        Plano AS pl
+INNER JOIN
+        SpotifyClone.Usuario AS us ON us.plano_id = pl.plano_id;
