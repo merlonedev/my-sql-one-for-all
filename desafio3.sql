f376fcc90cfb7ef1e9231d39682cd20c4d1fa0c9
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-28 17:53:43 -0300

    adiciona desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,17 @@
+drop view if exists SpotifyClone.historico_reproducao_usuarios;
+
+CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
+    SELECT 
+        u.usuario AS usuario, c.cancao AS nome
+    FROM
+        SpotifyClone.usuario AS u
+            INNER JOIN
+        SpotifyClone.usuario_historico AS us ON u.id = us.usuario_id
+            INNER JOIN
+        SpotifyClone.cancao AS c ON us.cancao_id = c.id
+    ORDER BY u.usuario , c.cancao;
+
+SELECT 
+    *
+FROM
+    SpotifyClone.historico_reproducao_usuarios;
