1a543d4740794704615e050d363ea952dd301f50
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-28 17:11:14 -0300

    Resolve requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+CREATE VIEW historico_reproducao_usuarios AS
+  (SELECT 
+      u.nome AS 'usuario', m.nome AS 'nome'
+  FROM
+      SpotifyClone.usuarios AS u
+          JOIN
+      usuario_historico AS uh ON uh.usuario_id = u.usuario_id
+          JOIN
+      SpotifyClone.musicas AS m ON m.musica_id = uh.musica_id
+  ORDER BY u.nome , m.nome);
+    
