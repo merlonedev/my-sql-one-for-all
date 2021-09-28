539c998e451a176b77be052bdb2430a3cda026a3
ArturAlvaro <artur.alvaro73@gmail.com>
2021-09-28 17:10:04 -0300

    [ Requisito 5 ]

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT 
+	c.cancao, COUNT(h.cancao_id) reproducoes
+FROM
+    SpotifyClone.historico h
+        INNER JOIN
+    SpotifyClone.cancoes c ON c.cancao_id = h.cancao_id
+GROUP BY cancao
+ORDER BY reproducoes DESC , cancao
+LIMIT 2;
