80d347a1f60bfb26f668fce91fa4f3b92b92c869
julio11lp <julio11lp@hotmail.com>
2021-09-28 17:43:23 -0300

    Correcao do lint desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-	BEFORE DELETE ON SpotifyClone.Usuario 
-		FOR EACH ROW
-			BEGIN
-				DELETE FROM SpotifyClone.Seguindo WHERE usuario_id = OLD.usuario_id;
-                DELETE FROM SpotifyClone.Historico WHERE usuario_id = OLD.usuario_id;
-			END $$
+BEFORE DELETE ON SpotifyClone.Usuario 
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.Seguindo WHERE OLD.usuario_id = SpotifyClone.Seguindo.usuario_id;
+DELETE FROM SpotifyClone.Historico WHERE OLD.usuario_id = SpotifyClone.Seguindo.usuario_id;
+END $$
 DELIMITER ;
