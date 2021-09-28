f73c316bc0c9330b65393537b59a8d67a644fde3
Daniel <danielbped@gmail.com>
2021-09-28 18:08:08 -0300

    Resolve requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+	DELETE FROM usuario_historico WHERE usuario_id = old.id;
+    DELETE FROM usuario_artista WHERE usuario_id = old.id;
+END $$;
+DELIMITER ;
