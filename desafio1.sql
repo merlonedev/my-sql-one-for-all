cbb0cc03655e1a9f2e4f9e6a05eea2f068fc17b7
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-09-28 15:40:14 -0300

    adicionando dados as tabelas no sql

--- desafio1.sql
+++ desafio1.sql
@@ -38,28 +38,94 @@ CREATE TABLE cancoes(
 ) engine = InnoDB;
 
 CREATE TABLE cancoes_reproduzidas_usuario(
-    usuario_id TINYINT,
-    cancao_id TINYINT,
+    usuario_id TINYINT NOT NULL,
+    cancao_id TINYINT NOT NULL,
     PRIMARY KEY (usuario_id, cancao_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
     FOREIGN KEY (cancao_id) REFERENCES cancoes(cancao_id)
 ) engine = InnoDB;
 
 CREATE TABLE artistas_seguidos_usuario(
-    usuario_id TINYINT,
-    artista_id TINYINT,
+    usuario_id TINYINT NOT NULL,
+    artista_id TINYINT NOT NULL,
     PRIMARY KEY (usuario_id, artista_id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
     FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
 
-INSERT INTO tabela1 (coluna1, coluna2)
+INSERT INTO planos (plano, valor_plano)
 VALUES
-  ('exemplo de dados 1', 'exemplo de dados A'),
-  ('exemplo de dados 2', 'exemplo de dados B'),
-  ('exemplo de dados 3', 'exemplo de dados C');
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
 
-INSERT INTO tabela2 (coluna1, coluna2)
+INSERT INTO usuarios(usuario, idade, plano_id)
 VALUES
-  ('exemplo de dados 1', 'exemplo de dados X'),
-  ('exemplo de dados 2', 'exemplo de dados Y');
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+
+INSERT INTO artistas (artista)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shanno');
+
+INSERT INTO albuns (album, artista_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+
+INSERT INTO cancoes (cancao, album_id)
+VALUES
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
+  
+INSERT INTO cancoes_reproduzidas_usuario (usuario_id, cancao_id)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
+  (2, 2),
+  (2, 15),
+  (3, 4),
+  (3, 16),
+  (3, 6),
+  (4, 3),
+  (4, 18),
+  (4, 11);
+  
+INSERT INTO artistas_seguidos_usuario (usuario_id, artista_id)
+VALUES
+  (1, 1),
+  (1, 4),
+  (1, 3),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4);
