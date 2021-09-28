7267cecf0b1f148329b568c0069e10779b69db30
AndreHammel <hammel.engineer@gmail.com>
2021-09-28 19:44:06 -0300

    Update desafio6.sql
    tirado os tabs

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
 CREATE VIEW faturamento_atual AS
 SELECT 
-	MIN(p.valor) AS faturamento_minimo,
-	MAX(p.valor) AS faturamento_maximo,
-	concat(round(avg(p.valor), 2), '0') AS faturamento_medio,
-	concat(round(SUM(p.valor),2), '') AS faturamento_total
+MIN(p.valor) AS faturamento_minimo,
+MAX(p.valor) AS faturamento_maximo,
+concat(round(avg(p.valor), 2), '0') AS faturamento_medio,
+concat(round(SUM(p.valor),2), '') AS faturamento_total
 FROM
-	USUARIO u
-		JOIN
-	PLANO p ON u.plano_id = p.plano_id;
+USUARIO u
+JOIN
+PLANO p ON u.plano_id = p.plano_id;
