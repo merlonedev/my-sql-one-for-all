be01bde168089d0ace0d115e5b492aca74e56445
AndreHammel <hammel.engineer@gmail.com>
2021-09-28 19:44:50 -0300

    Update desafio4.sql
    retirado os tabs e colocado uma nova linha no final

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,11 @@
 CREATE VIEW top_3_artistas AS
-    SELECT 
-        a.nome_artista AS artista,
-        COUNT(ua.artista_id) AS seguidores
-    FROM
-        ARTISTAS a
-            JOIN
-        USUARIO_ARTISTAS ua ON a.artista_id = ua.artista_id
-    GROUP BY artista
-    ORDER BY seguidores DESC , artista ASC
-    LIMIT 3;
-    
+SELECT 
+a.nome_artista AS artista,
+COUNT(ua.artista_id) AS seguidores
+FROM
+ARTISTAS a
+JOIN
+USUARIO_ARTISTAS ua ON a.artista_id = ua.artista_id
+GROUP BY artista
+ORDER BY seguidores DESC , artista ASC
+LIMIT 3;
