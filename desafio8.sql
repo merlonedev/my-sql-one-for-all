ecc9a996db92c2a729604f5ede9351e0dbc23191
RyanLaiber <ryanlaiber@gmail.com>
2021-09-28 17:33:09 -0300

    add req 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+	DELETE FROM relacao_seguidores WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM historico_reproducao WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
