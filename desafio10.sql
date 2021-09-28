4eb27d21d2a017deda2e575d7a7cffe0d96a0cd7
Caio Morato <morato_dev@protonmail.com>
2021-09-28 07:42:13 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(codigo_usuario INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE qtd_musicas INT;
+	SELECT 
+    COUNT(*)
+FROM
+    musicas_reproduzidas
+WHERE
+    usuario_id = codigo_usuario
+GROUP BY usuario_id INTO qtd_musicas;
+    RETURN qtd_musicas;
+END $$
+
+DELIMITER ;
