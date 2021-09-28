c8f25f730d60a3e5ddbf940a09e65ae0cdbe912c
Erick Silva <erick.silva6653@gmail.com>
2021-09-28 20:38:55 -0300

    code climate

--- desafio3.sql
+++ desafio3.sql
@@ -7,4 +7,4 @@ CREATE VIEW historico_reproducao_usuarios AS
         SpotifyClone.users_history h ON h.user_id = u.id
             JOIN
         SpotifyClone.songs s ON s.id = h.song_id
-	ORDER BY usuario, nome;
+ORDER BY usuario, nome;
