a5853f8aecec326a0982a704949ea6143b45b717
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-09-28 18:45:27 -0300

    tenta corrigir desafios 2 e 3 de novo

--- desafio2.sql
+++ desafio2.sql
@@ -1,12 +1,10 @@
-DROP VIEW IF EXISTS SpotifyClone.estatisticas_musicais;
-
-CREATE VIEW SpotifyClone.estatisticas_musicais AS
+CREATE VIEW estatisticas_musicais AS
     (SELECT
-        (SELECT COUNT(*) FROM SpotifyClone.cancao) AS 'cancoes',
-        (SELECT COUNT(*) FROM SpotifyClone.artista) AS 'artistas',
-        (SELECT COUNT(*) FROM SpotifyClone.album) AS 'albuns');
+        (SELECT COUNT(*) FROM cancao) AS 'cancoes',
+        (SELECT COUNT(*) FROM artista) AS 'artistas',
+        (SELECT COUNT(*) FROM album) AS 'albuns');
 
 SELECT 
     *
 FROM
-    SpotifyClone.estatisticas_musicais;
+    estatisticas_musicais;
