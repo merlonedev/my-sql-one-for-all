363062001307a477889082aad89f559039f97ac0
Uriel <urielbochi@gmail.com>
2021-09-28 17:37:15 -0300

    test req 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 CREATE VIEW cancoes_premium AS
-SELECT m.musica_nome as 'nome', COUNT(h.usuario_id) AS `reproducoes` from spotifyclone.historico as h
-INNER JOIN spotifyclone.musica AS m ON m.musica_id = h.musica_id
-INNER JOIN spotifyclone.usuario AS us ON us.usuario_id = h.usuario_id
+SELECT m.musica_nome as 'nome', COUNT(h.usuario_id) AS `reproducoes` from SpotifyClone.historico as h
+INNER JOIN SpotifyClone.Musica AS m ON m.musica_id = h.musica_id
+INNER JOIN SpotifyClone.Usuario AS us ON us.usuario_id = h.usuario_id
 WHERE plano_id = 2 or plano_id = 3
 GROUP BY `nome`
 ORDER BY `nome`;
