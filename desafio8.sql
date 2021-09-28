9c49139cddc848efefb8b84e7f94efad24628bf8
Gabriel Takaki <gabitj@icloud.com>
2021-09-28 19:38:34 -0300

    codeclimate erro

--- desafio8.sql
+++ desafio8.sql
@@ -3,11 +3,10 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON Usuario
 FOR EACH ROW
 BEGIN
-	DELETE FROM Historico_reproducao AS hr
-    WHERE hr.usuario_id = OLD.id;
-    
-    DELETE FROM Seguindo_artistas AS sa
-    WHERE sa.usuario_id = OLD.id;
+DELETE FROM Historico_reproducao AS hr
+WHERE hr.usuario_id = OLD.id;
+DELETE FROM Seguindo_artistas AS sa
+WHERE sa.usuario_id = OLD.id;
 END $$
 
 DELIMITER ;
