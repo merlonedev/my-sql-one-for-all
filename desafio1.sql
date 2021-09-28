4f07da67348a253cd00219bfe4b7ba2114c88c61
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-09-28 17:25:09 -0300

    cria tabela users

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,10 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
+
+CREATE TABLE users(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  user_name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  plan_id INT NOT NULL
+) engine = innoDB;
