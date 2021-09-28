fc2845a9cb6f500ee112c3c2d4003aab233b72f8
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-28 15:34:04 -0300

    codeclimate

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,11 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-	BEFORE DELETE ON SpotifyClone.users
-    FOR EACH ROW
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.user_artist
-    WHERE user_id = OLD.id;
-    DELETE FROM SpotifyClone.user_song
-    WHERE user_id = OLD.id;
+DELETE FROM SpotifyClone.user_artist
+WHERE user_id = OLD.id;
+DELETE FROM SpotifyClone.user_song
+WHERE user_id = OLD.id;
 END $$
 DELIMITER ;
-
-DELETE FROM SpotifyClone.users WHERE id = 1;
