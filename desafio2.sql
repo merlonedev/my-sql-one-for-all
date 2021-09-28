af9187f074477b8394e1fa8c3b2535740879066e
Wellington <junior.wel02@exemplo.br>
2021-09-28 14:36:21 -0300

    fix desafio2 tirado o select *

--- desafio2.sql
+++ desafio2.sql
@@ -2,8 +2,3 @@ CREATE VIEW estatisticas_musicais AS SELECT
 (SELECT COUNT(*) FROM SpotifyClone.songs) AS cancoes,
 (SELECT COUNT(*) FROM SpotifyClone.artists) AS artistas,
 (SELECT COUNT(*) FROM SpotifyClone.albums) AS albuns;
-
-SELECT 
-    *
-FROM
-    estatisticas_musicais
