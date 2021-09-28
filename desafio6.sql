bedc7ab0884f81d203335b97e2523ac403223b4d
Uriel <urielbochi@gmail.com>
2021-09-28 12:40:45 -0300

    feito desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        ROUND(MIN(plan.plano_preco), 2) AS `faturamento_minimo`,
+        ROUND(MAX(plan.plano_preco), 2) AS `faturamento_maximo`,
+        ROUND(AVG(plan.plano_preco), 2) AS `faturamento_medio`,
+        ROUND(SUM(plan.plano_preco), 2) AS `faturamento_total`
+    FROM
+        Plano AS plan
+ INNER JOIN
+        SpotifyClone.Usuario AS us ON us.plano_id = plan.plano_id;
