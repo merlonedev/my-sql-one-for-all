980440d236d378dfeba4f7e2414a57dfdb585766
Gabriel Takaki <gabitj@icloud.com>
2021-09-28 16:37:14 -0300

    Requsito 1

--- desafio1.sql
+++ desafio1.sql
@@ -12,9 +12,10 @@ CREATE TABLE Plano (
 )  ENGINE = InnoDB;
 
 INSERT INTO Plano (valor_plano, plano)
-  VALUES (0, 'Gratuito'),
-		 (7.99, 'Familiar'),
-         (5.99, 'Universitário');
+VALUES
+	(0, 'Gratuito'),
+	(7.99, 'Familiar'),
+	(5.99, 'Universitário');
 
 CREATE TABLE Artista (
     id INT NOT NULL AUTO_INCREMENT,
@@ -23,10 +24,11 @@ CREATE TABLE Artista (
 )  ENGINE = InnoDB;
 
 INSERT INTO Artista (artista_nome)
-	VALUES ('Walter Phoenix'),
-		   ('Peter Strong'),
-           ('Lance Day'),
-           ('Freedie Shannon');
+VALUES
+	('Walter Phoenix'),
+	('Peter Strong'),
+	('Lance Day'),
+	('Freedie Shannon');
 
 
 CREATE TABLE Album (
@@ -39,11 +41,12 @@ CREATE TABLE Album (
 )  ENGINE = InnoDB;
 
 INSERT INTO Album (album, artista_id)
-	VALUES ('Envious', 1),
-		   ('Exuberant', 1),
-           ('Hallowed Steam', 2),
-           ('Incandescent', 3),
-           ('Temporary Culture', 4);
+VALUES
+	('Envious', 1),
+	('Exuberant', 1),
+	('Hallowed Steam', 2),
+	('Incandescent', 3),
+	('Temporary Culture', 4);
 
 CREATE TABLE Usuario (
     id INT NOT NULL AUTO_INCREMENT,
@@ -56,10 +59,11 @@ CREATE TABLE Usuario (
 )  ENGINE = InnoDB;
 
 INSERT INTO Usuario (usuario, idade, plano_id)
-	VALUES ('Thati', 23, 1),
-		   ('Cintia', 35, 2),
-           ('Bill', 20, 3),
-           ('Roger', 45, 1);
+VALUES
+	('Thati', 23, 1),
+	('Cintia', 35, 2),
+	('Bill', 20, 3),
+	('Roger', 45, 1);
 
 CREATE TABLE Musicas (
     id INT NOT NULL AUTO_INCREMENT,
@@ -74,24 +78,25 @@ CREATE TABLE Musicas (
 )  ENGINE = InnoDB;
 
 INSERT INTO Musicas (artista_id, album_id, musica)
-	VALUES(1, 1, 'Soul For Us'),
-		  (1, 1, 'Reflections Of Magic'),
-          (1, 1, 'Dance With Her Own'),
-          (1, 2, 'Troubles Of My Inner Fire'),
-          (1, 2, 'Time Fireworks'),
-          (2, 3, 'Magic Circus'),
-          (2, 3, 'Honey, So Do I'),
-          (2, 3, "Sweetie, Let's Go Wild"),
-          (2, 3, 'She Knows'),
-          (3, 4, 'Fantasy For Me'),
-          (3, 4, 'Celebration Of More'),
-          (3, 4, 'Rock His Everything'),
-          (3, 4, 'Home Forever'),
-          (3, 4, 'Diamond Power'),
-          (3, 4, "Honey, Let's Be Silly"),
-          (4, 5, 'Thang Of Thunder'),
-          (4, 5, 'Words Of Her Life'),
-          (4, 5, 'Without My Streets');
+VALUES
+	(1, 1, 'Soul For Us'),
+	(1, 1, 'Reflections Of Magic'),
+	(1, 1, 'Dance With Her Own'),
+	(1, 2, 'Troubles Of My Inner Fire'),
+	(1, 2, 'Time Fireworks'),
+	(2, 3, 'Magic Circus'),
+	(2, 3, 'Honey, So Do I'),
+	(2, 3, "Sweetie, Let's Go Wild"),
+	(2, 3, 'She Knows'),
+	(3, 4, 'Fantasy For Me'),
+	(3, 4, 'Celebration Of More'),
+	(3, 4, 'Rock His Everything'),
+	(3, 4, 'Home Forever'),
+	(3, 4, 'Diamond Power'),
+	(3, 4, "Honey, Let's Be Silly"),
+	(4, 5, 'Thang Of Thunder'),
+	(4, 5, 'Words Of Her Life'),
+	(4, 5, 'Without My Streets');
 
 CREATE TABLE Seguindo_artistas (
     usuario_id INT NOT NULL,
@@ -104,14 +109,15 @@ CREATE TABLE Seguindo_artistas (
 )  ENGINE = InnoDB;
 
 INSERT INTO Seguindo_artistas (usuario_id, artista_id)
-	VALUES (1, 1),
-		   (1, 4),
-           (1, 3),
-           (2, 1),
-           (2, 3),
-           (3, 2),
-           (3, 1),
-           (4, 4);
+VALUES
+	(1, 1),
+	(1, 4),
+	(1, 3),
+	(2, 1),
+	(2, 3),
+	(3, 2),
+	(3, 1),
+	(4, 4);
 
 CREATE TABLE Historico_reproducao (
     usuario_id INT NOT NULL,
@@ -124,28 +130,18 @@ CREATE TABLE Historico_reproducao (
 )  ENGINE = InnoDB;
 
 INSERT INTO Historico_reproducao (usuario_id, musica_id)
-	VALUES (1, 1),
-		   (1, 6),
-           (1, 16),
-           (2, 13),
-           (2, 17),
-           (2, 2),
-           (2, 15),
-           (3, 4),
-           (3, 16),
-           (3, 6),
-           (4, 3),
-           (4, 18),
-           (4, 11);
-
-
-
-
-
-
-
-
-
-
-
-
+VALUES
+	(1, 1),
+	(1, 6),
+    (1, 14),
+    (1, 16),
+	(2, 13),
+	(2, 17),
+	(2, 2),
+	(2, 15),
+	(3, 4),
+	(3, 16),
+	(3, 6),
+	(4, 3),
+	(4, 18),
+	(4, 11);
