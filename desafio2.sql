cd0758389168b022c0e89eb3daa22a9997c5d1f6
Luciano Lanes <llaneslopes@gmail.com>
2021-09-28 14:55:48 -0300

    Desafio 2 concluído

---
+++ desafio2.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        COUNT(id) AS cancoes,
+        (SELECT 
+                COUNT(id)
+            FROM
+                artist) AS artistas,
+        (SELECT 
+                COUNT(id)
+            FROM
+                album) AS albuns
+    FROM
+        songs;
