e1f5569f05dd69a79cb2f38f83e8a1a70b0e1e50
Elias Forte <eliasforte.dev@gmail.com>
2021-09-27 22:02:38 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        COUNT(s.song_id) AS cancoes,
+        COUNT(DISTINCT a.artist) AS artistas,
+        COUNT(DISTINCT al.album) AS albuns
+    FROM
+        SpotifyClone.songs AS s
+            JOIN
+        SpotifyClone.artists AS a ON s.artist_id = a.artist_id
+            JOIN
+        SpotifyClone.albuns AS al ON al.album_id = s.album_id;
