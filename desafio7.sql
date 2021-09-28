ef6dabbff981395c37b893bc4578d3c1d4b14cc2
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-09-28 15:32:18 -0300

    codeclimate

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 CREATE VIEW perfil_artistas AS
-  SELECT
-	art.name AS `artista`,
-  alb.name AS `album`,
-  COUNT(ua.artist_id) AS `seguidores`
+SELECT
+art.name AS `artista`,
+alb.name AS `album`,
+COUNT(ua.artist_id) AS `seguidores`
 FROM SpotifyClone.albums AS alb
 JOIN SpotifyClone.artists AS art ON alb.artist_id = art.id
 JOIN SpotifyClone.user_artist AS ua ON alb.artist_id = ua.artist_id
--- desafio9.sql
+++ desafio9.sql
@@ -2,14 +2,14 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN artistName VARCHAR(60))
 BEGIN
-	DECLARE artistID INT;
-    SELECT id FROM SpotifyClone.artists WHERE name LIKE artistName INTO artistID;
-    SELECT
-        art.name AS `artista`,
-        alb.name AS `album`
-	FROM SpotifyClone.albums AS alb
-		JOIN SpotifyClone.artists AS art ON alb.artist_id = art.id
-    WHERE artist_id = artistID;
+DECLARE artistID INT;
+SELECT id FROM SpotifyClone.artists WHERE name LIKE artistName INTO artistID;
+SELECT
+art.name AS `artista`,
+alb.name AS `album`
+FROM SpotifyClone.albums AS alb
+JOIN SpotifyClone.artists AS art ON alb.artist_id = art.id
+WHERE artist_id = artistID;
 END $$
 
 DELIMITER ;
