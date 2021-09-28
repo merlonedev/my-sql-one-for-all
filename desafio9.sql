5627a2c481980926a452b66d70324c2cb3d4a7b0
Daniel <danielbped@gmail.com>
2021-09-28 18:16:15 -0300

    climate

--- desafio9.sql
+++ desafio9.sql
@@ -2,13 +2,13 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(50))
 BEGIN
-	SELECT a.nome AS `artista`,
-    al.nome AS `album`
-    FROM artistas AS a
-    INNER JOIN albuns AS al
-    ON al.artista_id = a.id
-    WHERE a.nome LIKE nome
-    ORDER BY `album`;
+SELECT a.nome AS `artista`,
+al.nome AS `album`
+FROM artistas AS a
+INNER JOIN albuns AS al
+ON al.artista_id = a.id
+WHERE a.nome LIKE nome
+ORDER BY `album`;
 END $$
 
 DELIMITER ;
