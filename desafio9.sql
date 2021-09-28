f9abaa17de691daf37a10ecf9805fda0bc2f335b
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-28 19:27:02 -0300

    Resolve requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(50))
+BEGIN
+	SELECT a.nome AS 'artista', al.nome AS 'album'
+    FROM SpotifyClone.artista AS a
+    JOIN
+    SpotifyClone.albuns AS al ON a.artista_id = al.artista_id
+    WHERE a.nome = nome_artista
+    ORDER BY al.nome;
+END $$
+
+DELIMITER ;
