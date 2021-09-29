c6ce36f6f1c5114ef9ce780c03c4a63d3c9e7595
Matheus Proença <mproenca.marketing@gmail.com>
2021-09-29 16:10:00 -0300

    requisito 07corrigindo espaços

--- desafio7.sql
+++ desafio7.sql
@@ -3,11 +3,11 @@ SELECT
 name AS artista,
 title AS album,
 COUNT(user_id) AS seguidores
- FROM
- SpotifyClone.artists AS a
- INNER JOIN
- SpotifyClone.albums AS ab ON ab.artist_id = a.artist_id
- INNER JOIN
- SpotifyClone.users_artists AS ua ON ua.artist_id = a.artist_id
- GROUP BY artista, album
- ORDER BY seguidores DESC, artista ASC, album ASC;
+FROM
+SpotifyClone.artists AS a
+INNER JOIN
+SpotifyClone.albums AS ab ON ab.artist_id = a.artist_id
+INNER JOIN
+SpotifyClone.users_artists AS ua ON ua.artist_id = a.artist_id
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista ASC, album ASC;
