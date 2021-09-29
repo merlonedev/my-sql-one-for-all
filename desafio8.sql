9702a4e538c1f292c35713abae8a6470dd543cf0
Guilherme Cesconeto <cesconeto@gmail.com>
2021-09-29 11:05:10 -0300

    Fazendo a 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON SpotifyClone.users FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.user_artists
+  WHERE id_user = OLD.id_user;
+  DELETE FROM SpotifyClone.user_songs
+  WHERE id_user = OLD.id_user;
+END $$
+DELIMITER ;
