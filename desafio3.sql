dd07d64ad0c6ddf09c50f57cba653af65e5586c1
Daniel Ceci <danielceci96@gmail.com>
2021-09-29 11:29:18 -0300

    tentando o req 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+ SELECT
+ u.Nome AS 'usuario',
+ c.Nome_Cancao AS 'nome'
+ FROM SpotifyClone.Historico h 
+ INNER JOIN SpotifyClone.Usuario u ON h.Id_Usuario = u.Id -- quando criei as tabelas eu deveria ter usado nomes melhores e mais iguais entre tabelas diferentes,
+ INNER JOIN SpotifyClone.Cancoes c ON h.Id_Cancao = c.Id
+ ORDER BY u.Nome, c.Nome_Cancao;
