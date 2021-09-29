e8b80f95dd1acdc194b3b709012f2d885dc96675
cristianocsl <cslcristiano@gmail.com>
2021-09-28 21:05:17 -0300

    desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,11 @@
+USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-    AFTER DELETE ON usuarios
+    AFTER DELETE ON SpotifyClone.usuarios
     FOR EACH ROW
 BEGIN
-    DELETE FROM artistas_usuarios WHERE usuario_id = OLD.usuario_id;
-    DELETE FROM histor_reprod WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.artistas_usuarios WHERE artistas_usuarios.usuario_id = OLD.usuarios.usuario_id;
+    DELETE FROM SpotifyClone.histor_reprod WHERE histor_reprod.usuario_id = OLD.usuarios.usuario_id;
 END $$
 DELIMITER ;
