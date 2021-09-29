91db68632d8ff0c6258dacf745fab3cc85398f1c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-09-28 21:26:40 -0300

    corrige erro de alias das subqueries do desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -2,7 +2,7 @@ USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
 SELECT
   COUNT(id) AS cancoes,
-  (SELECT COUNT(id) FROM artists) AS artistas,
-  (SELECT COUNT(Id) FROM albums)  AS albuns
+  (SELECT COUNT(id) FROM artists) AS `artistas`,
+  (SELECT COUNT(Id) FROM albums)  AS `albuns`
 FROM
   songs;
