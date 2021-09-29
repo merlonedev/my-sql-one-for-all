43d59f61053dcdf059b5218c285fa61910bc7e0f
Zevid-Lucas <zevid.dev@outlook.com>
2021-09-29 02:47:47 -0300

    Fix codeClimate(2)

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
-CREATE VIEW faturamento_atual AS
-  SELECT ROUND(MIN(valor),2) AS faturamento_minimo,
+CREATE VIEW faturamento_atual AS SELECT 
+  ROUND(MIN(valor),2) AS faturamento_minimo,
   ROUND(MAX(valor),2) AS faturamento_maximo,
   ROUND(AVG(valor),2) AS faturamento_medio,
   ROUND(SUM(valor),2) AS faturamento_total
