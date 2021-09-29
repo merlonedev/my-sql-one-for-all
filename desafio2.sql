5d2399c89d3f8257ad40d88d456883b9126243f5
thales <thalesdaniel2013@gmail.com>
2021-09-29 16:01:23 -0300

    feito etapa 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS SELECT 
-COUNT(SpotifyClone.musica_id) AS cancoes, 
-COUNT(DISTINCT SpotifyClone.artista_id) AS artistas, 
-COUNT(DISTINCT SpotifyClone.album_id) AS albuns 
-FROM SpotifyClone.Musicas;
+COUNT(musica_id) AS cancoes, 
+COUNT(DISTINCT artista_id) AS artistas, 
+COUNT(DISTINCT album_id) AS albuns 
+FROM Musicas;
---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+CREATE VIEW historico_reproducao_usuarios AS SELECT 
+u.usuario AS usuario,
+m.musica AS nome
+FROM Usuario AS u
+JOIN Historico_de_reproducao AS hd
+ON hd.usuario_id = u.usuario_id
+JOIN Musicas AS m
+ON m.musica_id = hd.musica_id
+ORDER BY usuario, nome;
