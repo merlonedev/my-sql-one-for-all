311c8e7374bcb07096a517f278e13419fb15f1d7
André Moreno <deco616@hotmail.com>
2021-09-29 02:14:26 -0300

    Desafio 9 AJUSTE

--- desafio9.sql
+++ desafio9.sql
@@ -2,15 +2,15 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN artistaNome VARCHAR(100))
 BEGIN
-    SELECT
-		nome_artista AS 'artista',
-		nome_album AS 'album'
-	FROM
-		SpotifyClone.albuns AS al
-			INNER JOIN
-		SpotifyClone.artistas AS a ON al.artist_id = a.artista_id
-	WHERE nome_artista = artistaNome
-	ORDER BY album;
+SELECT 
+    nome_artista AS 'artista', nome_album AS 'album'
+FROM
+    SpotifyClone.albuns AS al
+        INNER JOIN
+    SpotifyClone.artistas AS a ON al.artist_id = a.artista_id
+WHERE
+    nome_artista = artistaNome
+ORDER BY album;
 END $$
 
 DELIMITER ;
