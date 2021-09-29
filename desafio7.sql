a7b8e1af58ff73268eafaf8a8d425ce4c24684b0
Euller Braz <euller.braz99@gmail.com>
2021-09-29 10:26:19 -0300

    Desafio 7 - Criação de view para visualização do perfil do artista

---
+++ desafio7.sql
@@ -0,0 +1,19 @@
+CREATE VIEW perfil_artistas AS
+(SELECT
+    ar.name AS `artista`,
+    al.name AS `album`,
+    (
+    SELECT
+        COUNT(user_id)
+    FROM
+        SpotifyClone.users_artists AS ua
+    WHERE
+        ar.id = ua.artist_id
+    GROUP BY
+        ua.artist_id
+    ) AS `seguidores`
+FROM
+    SpotifyClone.artists AS ar
+        JOIN
+	SpotifyClone.albums AS al ON ar.id = al.artist_id
+ORDER BY `seguidores` DESC, `artista`);
