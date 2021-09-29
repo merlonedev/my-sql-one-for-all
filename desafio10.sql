1f74195d1002f6393e292f1eca27fe0a7c4d2dbd
Erick Silva <erick.silva6653@gmail.com>
2021-09-29 11:07:28 -0300

    code climate

--- desafio10.sql
+++ desafio10.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico (id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total_songs INT;
-    SELECT COUNT(*) FROM SpotifyClone.users_history
-	WHERE user_id = id INTO total_songs;
-    RETURN total_songs;
+DECLARE total_songs INT;
+SELECT COUNT(*) FROM SpotifyClone.users_history
+WHERE user_id = id INTO total_songs;
+RETURN total_songs;
 END $$
 
 DELIMITER ;
