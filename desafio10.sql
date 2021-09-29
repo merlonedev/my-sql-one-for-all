27b9842078bdf42d33f5a924346073bb8c44f5f5
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-28 21:21:26 -0300

    Resolve requisito 10(ajustes no code climate)

--- desafio10.sql
+++ desafio10.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total_musicas INT;
+DECLARE total_musicas INT;
 SELECT 
     COUNT(*)
 FROM
