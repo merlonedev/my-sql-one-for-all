08d8f7829ceeabf10642178898d2d69b8accfefc
Erick Silva <erick.silva6653@gmail.com>
2021-09-28 21:02:14 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+CREATE VIEW perfil_artistas AS
+    SELECT 
+        a.name AS artista,
+        al.name AS album,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.users_follows f
+            WHERE
+                f.artist_id = a.id) AS seguidores
+    FROM
+        SpotifyClone.artists a
+            JOIN
+        SpotifyClone.albums al ON al.artist_id = a.id
+    ORDER BY seguidores DESC , artista , album;
