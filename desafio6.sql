22dc6babb1ef3a9e663537060c802e360cc01d02
Zevid-Lucas <zevid.dev@outlook.com>
2021-09-29 02:45:40 -0300

    Done challenge6, create VIEW faturamento_atual

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+  SELECT ROUND(MIN(valor),2) AS faturamento_minimo,
+  ROUND(MAX(valor),2) AS faturamento_maximo,
+  ROUND(AVG(valor),2) AS faturamento_medio,
+  ROUND(SUM(valor),2) AS faturamento_total
+FROM SpotifyClone.Plano AS p
+INNER JOIN SpotifyClone.Usuario AS u
+ON u.plano_id = p.plano_id;
