3a9199ca9f6c70fb6e62a3fec152a95efcaf77b1
Abner <gedeaoabner@gmail.com>
2021-09-28 21:13:16 -0300

    remove tabs do desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -5,7 +5,7 @@ BEGIN
     SELECT
         art.nome AS `artista`,
         alb.nome AS `album`
-	FROM SpotifyClone.artistas AS art
+  FROM SpotifyClone.artistas AS art
     JOIN SpotifyClone.albuns AS alb
     ON art.artista_id = alb.artista_id WHERE art.nome = artista ORDER BY `album` ASC;
 END $$
