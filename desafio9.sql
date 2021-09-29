4c72a34d8c5b5df1848ea13eafe9320e6041e1bf
Patrick Dack <patrickdack89@gmail.com>
2021-09-29 01:42:51 -0300

    corrige climate

--- desafio9.sql
+++ desafio9.sql
@@ -1,16 +1,15 @@
-USE SpotifyClone;
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista (IN artista VARCHAR(45))
 BEGIN
-	SELECT
-		a.name AS artista,
-        al.title AS album
-	FROM
-		SpotifyClone.artists AS a
-			INNER JOIN
-		SpotifyClone.albuns AS al ON al.artist_id = a.artist_id
-	WHERE
-		a.name LIKE CONCAT('%', artista, '%' )
-	ORDER BY `album`;
+SELECT
+a.name AS artista,
+al.title AS album
+FROM
+SpotifyClone.artists AS a
+INNER JOIN
+SpotifyClone.albuns AS al ON al.artist_id = a.artist_id
+WHERE
+a.name LIKE CONCAT('%', artista, '%' )
+ORDER BY `album`;
 END $$
 DELIMITER ;
