270399a6986e73bec8d77f090880c2690885f61f
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-09-29 13:37:21 -0300

    completa req 08

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON SpotifyClone.usuarios
+    FOR EACH ROW
+BEGIN
+    DELETE FROM SpotifyClone.historico_musicas
+    WHERE usuario_id = OLD.id;
+    
+    DELETE FROM SpotifyClone.seguidores
+    WHERE usuario_id = OLD.id;
+END $$    
+
+DELIMITER ;
