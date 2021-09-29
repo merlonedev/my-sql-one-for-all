05853f9a8093be89b52b82a904ba49eac3418480
cristianocsl <cslcristiano@gmail.com>
2021-09-28 22:32:38 -0300

    desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -5,7 +5,7 @@ CREATE FUNCTION quantidade_musicas_no_historico(num INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE num_rec INT;
-    SELECT COUNT(h.usuario_id) AS qtd
+    SELECT COUNT(h.cancao_id) AS qtd
     FROM SpotifyClone.histor_reprod AS h
     WHERE h.usuario_id = num
     INTO num_rec;
