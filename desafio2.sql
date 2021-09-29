05deb0c66e8e93db50bbd85340b7d2c96e9a8e54
Marcos Mantovani <mvmes23@gmail.com>
2021-09-29 13:39:16 -0300

    desafio 2 ok

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+CREATE VIEW estatisticas_musicais AS 
+	SELECT
+		COUNT(nome_cancao) AS 'cancoes',
+        COUNT(DISTINCT artista_id) AS 'artistas',
+        COUNT(DISTINCT album_id) AS 'albuns'
+	FROM
+		SpotifyClone.cancoes;
+    
