50fc8d2775aeadc76644895f71a10d9d8012c31e
Matheus Proença <mproenca.marketing@gmail.com>
2021-09-29 15:35:35 -0300

    requisito 06

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE VIEW faturamento_atual AS 
+SELECT 
+ROUND(MIN(value), 2) AS faturamento_minimo,
+ROUND(MAX(value), 2) AS faturamento_maximo,
+ROUND(AVG(value), 2) AS faturamento_medio,
+ROUND(SUM(value), 2)AS faturamento_total
+FROM
+SpotifyClone.plans AS p
+JOIN
+SpotifyClone.users AS u ON u.plan_id = p.plan_id;
