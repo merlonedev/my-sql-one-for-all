baef085d03254c24e52a710d7e49f35df1d80d7e
Euller Braz <euller.braz99@gmail.com>
2021-09-29 11:34:39 -0300

    Desafio 10 - Criação de view que mostra a quantidade de músicas em um histórico de um usuário

---
+++ desafio10.sql
@@ -0,0 +1,24 @@
+DELIMITER $
+
+CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico(
+	id_usuario INT
+)
+RETURNS INT READS SQL DATA
+
+BEGIN
+    DECLARE quantity INT;
+
+    SELECT
+        COUNT(us.song_id)
+    FROM
+        SpotifyClone.users_songs AS us
+    WHERE
+        us.user_id = id_usuario
+    INTO quantity;
+
+    RETURN quantity;
+END $
+
+DELIMITER ;
+
+SELECT SpotifyClone.quantidade_musicas_no_historico(3);
