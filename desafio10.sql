3f12dfe4edefc79585269edf914047f497261765
Zevid-Lucas <zevid.dev@outlook.com>
2021-09-29 04:07:11 -0300

    Done challenge10, create function quantidade_musicas_no_historico

---
+++ desafio10.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE total_music INT;
+  SELECT COUNT(cancao_id)
+  FROM SpotifyClone.Usuario_Cancao
+  WHERE usuario_id = user_id
+  INTO total_music;
+  RETURN total_music;  
+END $$
+
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
