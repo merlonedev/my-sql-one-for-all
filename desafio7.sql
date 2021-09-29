e4feafaf3242d99213e6d9e2b2415d4b1388f1a5
Patrick Dack <patrickdack89@gmail.com>
2021-09-29 00:55:38 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artistas AS
+    SELECT
+        a.name AS artista,
+        al.title AS album,
+        COUNT(ua.user_id) AS seguidores
+    FROM
+        SpotifyClone.artists AS a
+            INNER JOIN
+        SpotifyClone.albuns AS al ON al.artist_id = a.artist_id
+            INNER JOIN
+        SpotifyClone.user_artist AS ua ON ua.artist_id = a.artist_id
+    GROUP BY `album` , `artista`
+    ORDER BY `seguidores` DESC , `artista`;
