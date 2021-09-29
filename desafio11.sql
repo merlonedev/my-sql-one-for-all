280f2c4fab830b3507fe833d9baa235d830cd78f
Renan Ferreira <renansantiago2013@hotmail.com>
2021-09-28 21:57:33 -0300

    Resolve requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+CREATE VIEW cancoes_premium AS
+    (SELECT 
+        m.nome AS 'nome', COUNT(uh.musica_id) AS 'reproducoes'
+    FROM
+        SpotifyClone.usuario_historico AS uh
+            JOIN
+        SpotifyClone.usuarios AS u ON u.usuario_id = uh.usuario_id
+            JOIN
+        SpotifyClone.musicas AS m ON m.musica_id = uh.musica_id
+            JOIN
+        SpotifyClone.planos AS p ON p.plano_id = u.plano_id
+    WHERE
+        p.plano_id IN (2 , 3)
+    GROUP BY m.nome
+    ORDER BY m.nome);
