6f46c20b0cb3048a423130da7ee154851965b616
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-09-29 00:49:17 -0300

    conclui desafio11

---
+++ desafio11.sql
@@ -0,0 +1,23 @@
+SELECT 
+  S.song_name AS `nome`,
+  COUNT(US.song_id) AS `reproducoes`
+FROM
+  SpotifyClone.user_song AS US
+JOIN
+  songs AS S
+ON
+  US.song_id = S.id
+JOIN
+  users AS U
+ON
+  US.user_id = U.id
+JOIN
+  plans AS P
+ON
+  U.plan_id = P.id
+WHERE
+  P.plan_name IN('universitário', 'familiar')
+GROUP BY
+  US.song_id
+ORDER BY
+  `nome`;
