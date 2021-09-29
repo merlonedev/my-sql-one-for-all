a9022582daa49b0dc8912202cc9c73233a464d29
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-09-29 15:57:25 -0300

    req 7, sem subquery dessa vez =)

---
+++ desafio7.sql
@@ -0,0 +1,14 @@
+CREATE VIEW perfil_artistas AS
+    SELECT 
+        a.artista AS 'artista',
+        al.album AS 'album',
+        COUNT(s.usuario_id) AS 'seguidores'
+    FROM
+        SpotifyClone.artistas a
+            INNER JOIN
+        SpotifyClone.albuns al ON al.artista_id = a.artista_id
+            INNER JOIN
+        SpotifyClone.seguindo s ON s.artista_id = al.artista_id
+    GROUP BY `artista` , `album`
+    ORDER BY `seguidores` DESC , `artista` , `album`;
+    
