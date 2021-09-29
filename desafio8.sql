e9d4ba33c205a34d66cefa0424dc9270b39ee945
Guilherme Dias <gmd.delta@gmail.com>
2021-09-28 22:05:12 -0300

    [PT-BR]Resolvido requisito 8. [EN] Solved 8th requirement

---
+++ desafio8.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE TRIGGER `trigger_usuario_delete`     
+  BEFORE DELETE ON `users`     
+  FOR EACH ROW     
+BEGIN
+DELETE FROM user_artists_listened 
+WHERE 
+	User_ID = OLD.User_ID;
+    
+DELETE FROM user_songs_listened 
+WHERE
+    User_ID = OLD.User_ID;
+END $$
+
+DELIMITER ;
