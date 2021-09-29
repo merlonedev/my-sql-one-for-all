b3513c026034f4f2a72bafb2d82e12923cca8db4
jorda <jordan.rachid@gmail.com>
2021-09-29 16:02:54 -0300

    view das estatisticas

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
-USE SpotifyClone;
-
   CREATE VIEW estatisticas_musicais AS
   SELECT 
-    (SELECT COUNT(*) FROM cancao) AS cancoes,
-    (SELECT COUNT(*) FROM artista) AS artistas,
+    (SELECT COUNT(*) FROM SpotifyClone.cancao) AS cancoes,
+    (SELECT COUNT(*) FROM SpotifyClone.artista) AS artistas,
     COUNT(al.album_id) AS albuns
-  FROM album as al;
+  FROM SpotifyClone.album as al;
+
+SELECT * FROM estatisticas_musicais;
