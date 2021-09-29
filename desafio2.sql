18c00b39a00fadc167329641ee776b4914d8a181
camila-mp <ca.malvp@gmail.com>
2021-09-29 09:37:11 -0300

    conclui desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+-- Crie uma VIEW chamada estatisticas_musicais que exiba três colunas:
+-- *A primeira coluna deve exibir a quantidade total de canções. Dê a essa coluna o alias "cancoes".
+-- *A segunda coluna deve exibir a quantidade total de artistas e deverá ter o alias "artistas".
+-- *A terceira coluna deve exibir a quantidade de álbuns e deverá ter o alias "albuns".
+
+USE SpotifyClone;
+CREATE VIEW estatisticas_musicais AS
+    SELECT COUNT(cancao_id) AS `cancoes`,
+    (SELECT COUNT(artista_id) FROM SpotifyClone.artistas) AS `artistas`,
+    COUNT(DISTINCT album_id) AS `albuns`
+    FROM SpotifyClone.cancoes;
