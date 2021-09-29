98a18a3941ed71e5dfd1dff2806897330327ebd2
jorda <jordan.rachid@gmail.com>
2021-09-29 02:31:32 -0300

    script para criaçao do banco de dados

--- desafio1.sql
+++ desafio1.sql
@@ -16,7 +16,7 @@ USE SpotifyClone;
       FOREIGN KEY (plano_id) REFERENCES plano (plano_id)
     )ENGINE=INNODB;
     
-     CREATE TABLE artista(
+      CREATE TABLE artista(
       artista_id INT PRIMARY KEY AUTO_INCREMENT,
       artista_nome VARCHAR(50) NOT NULL
     )ENGINE=INNODB;
@@ -31,7 +31,7 @@ USE SpotifyClone;
     CREATE TABLE cancao(
       cancao_id INT PRIMARY KEY AUTO_INCREMENT,
       cancao_title VARCHAR(50) NOT NULL,
-	  album_id INT NOT NULL,
+      album_id INT NOT NULL,
       FOREIGN KEY (album_id) REFERENCES album (album_id)
     )ENGINE=INNODB;
     
@@ -46,7 +46,7 @@ USE SpotifyClone;
     CREATE TABLE seguindo(
       seguido_id INT PRIMARY KEY AUTO_INCREMENT,
       user_id INT NOT NULL,
-	  artista_id INT NOT NULL,
+      artista_id INT NOT NULL,
       FOREIGN KEY (user_id) REFERENCES usuario (user_id),
       FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
     )ENGINE=INNODB;
@@ -72,7 +72,7 @@ USE SpotifyClone;
       (2,17),
       (2,2),
       (2,15),
-	  (3,4),
+      (3,4),
       (3,16),
       (3,6),
       (4,3),
@@ -92,34 +92,33 @@ USE SpotifyClone;
     INSERT INTO artista (artista_nome) VALUES
       ("Walter Phoenix"),
       ("Peter Strong"),
-	  ("Lance Day"),
+      ("Lance Day"),
       ("Freedie Shannon");
       
     INSERT INTO album (album_title, artista_id) VALUES
       ("Envious", 1),
       ("Exuberant", 1),
-	  ("Hallowed Steam", 2),
+      ("Hallowed Steam", 2),
       ("Incandescent", 3),
-	  ("Temporary Culture", 4);
+      ("Temporary Culture", 4);
       
-	 INSERT INTO cancao (cancao_title, album_id) VALUES
-       ("Soul For Us", 1),
-       ( "Reflections Of Magic", 1),
-       ("Dance With Her Own", 1),
-       ("Troubles Of My Inner Fire", 2),
-       ("Time Fireworks" ,2),
-       ("Magic Circus", 3),
-       ("Honey, So Do I", 3),
-       ("Sweetie, Let's Go Wild", 3),
-       ("She Knows", 3),
-       ("Fantasy For Me", 4),
-       ("Celebration Of More", 4),
-       ("Rock His Everything", 4),
-       ("Home Forever", 4),
-       ("Diamond Power", 4),
-       ("Honey, Let's Be Silly", 4),
-       ("Thang Of Thunder", 5),
-       ("Words Of Her Life", 5),
-       ("Without My Streets", 5);
+    INSERT INTO cancao (cancao_title, album_id) VALUES
+        ("Soul For Us", 1),
+        ( "Reflections Of Magic", 1),
+        ("Dance With Her Own", 1),
+        ("Troubles Of My Inner Fire", 2),
+        ("Time Fireworks" ,2),
+        ("Magic Circus", 3),
+        ("Honey, So Do I", 3),
+        ("Sweetie, Let's Go Wild", 3),
+        ("She Knows", 3),
+        ("Fantasy For Me", 4),
+        ("Celebration Of More", 4),
+        ("Rock His Everything", 4),
+        ("Home Forever", 4),
+        ("Diamond Power", 4),
+        ("Honey, Let's Be Silly", 4),
+        ("Thang Of Thunder", 5),
+        ("Words Of Her Life", 5),
+        ("Without My Streets", 5);
       
-    
