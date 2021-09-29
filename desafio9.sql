faf41ccdf49c13a9b47cdec638ed4fddee8c9fd4
Guilherme Cesconeto <cesconeto@gmail.com>
2021-09-29 10:39:27 -0300

    Fazendo a 9

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN artist VARCHAR(64))
+BEGIN
+  SELECT 
+    ar.artist_name AS `artista`,
+    al.album_name AS `album`
+  FROM 
+    SpotifyClone.albums AS al
+  JOIN SpotifyClone.artists AS ar ON al.id_artist = al.id_artist
+  WHERE ar.artist_name = artist
+  ORDER BY `album`;
+END $$
+DELIMITER ;
